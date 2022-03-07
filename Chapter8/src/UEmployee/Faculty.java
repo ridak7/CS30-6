96cd03336eb7a84ae8f53406e227e89ffefea04b
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-07 09:36:10 -0700

    Updated 9:36AM

--- Chapter8/src/UEmployee/Faculty.java
+++ Chapter8/src/UEmployee/Faculty.java
@@ -1,11 +1,16 @@
 package UEmployee;
 
 public  class Faculty {
-	String storing;
+	String memberstoring;
 	String departmentname;
 	
-	public Faculty(String storing,String departmentname ) {
-		
-		
+	public Faculty(String storing,String dname ) {
+		memberstoring = storing;
+		departmentname = dname;
+	}
+	
+	public String toString() 
+	{
+		return(departmentname);
 	}
 }
