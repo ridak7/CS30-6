79a0df4912e35581494c91e00ffb23d189624866
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-07 10:21:55 -0700

    Updated

--- Chapter8/src/UEmployee/Employee.java
+++ Chapter8/src/UEmployee/Employee.java
@@ -3,15 +3,17 @@ package UEmployee;
 public class Employee 
 {
     private String firstName, lastName;
+    double salary;
     
-    public Employee(String fName, String lName)
+    public Employee(String fName, String lName, double sal )
     {
         firstName = fName;
         lastName = lName;
+        salary = sal;
     }
     
     public String toString()
     {
-        return(firstName+" "+lastName);
+        return(firstName+" "+lastName + "salary is " + salary);
     }
 }
