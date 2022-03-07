a322d323bfdc3cbbf8f6e8504b2ec51ec3ee9b07
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-07 09:44:24 -0700

    Updated 9:44AM

--- Chapter8/src/UEmployee/Staff.java
+++ Chapter8/src/UEmployee/Staff.java
@@ -1,5 +1,15 @@
 package UEmployee;
 
 public class Staff {
-
+	String jobtitle, memberstore;
+	
+	public Staff(String title, String storing ) {
+		jobtitle = title;
+		memberstore = storing;
+	}
+	
+	public String tostring() 
+	{
+		return(jobtitle);
+	}
 }
