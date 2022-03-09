bff9fd9e7d0bd04b190c7c55c1663a0e06b6078a
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-09 09:49:58 -0700

    Updated

--- Chapter8/src/UEmployee/University.java
+++ Chapter8/src/UEmployee/University.java
@@ -1,10 +1,34 @@
 package UEmployee;
 
+import java.util.Scanner;
+
 public class University {
+	
+	
+	public static Employee createemployee() 
+	{
+		String firstName = ("Enter first name: ");
+		String lastName = ("Enter last name: ");
+		int salary = Integer.valueOf("Enter salary: ");
+		
+		Scanner input = new Scanner(System.in);
+		
+		//System.out.println(firstName);
+		
+		return (firstName + " " + lastName + "salary is" + salary);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	public static void main (String[] args)
 	{
-		Employee e = new Employee();
+		//Employee e = new Employee();
 		
-		System.out.println(e.salary);
+		//System.out.println(e.salary);
 	}
 }
